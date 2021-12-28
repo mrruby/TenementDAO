9cb89d83328e11cd8ae229d4180571b60f658aaf
Dawid Urbas <durbas92@gmail.com>
2021-12-28 17:17:35 +0100

    Fix info

--- src/App.jsx
+++ src/App.jsx
@@ -208,6 +208,7 @@ const App = () => {
     return (
       <div className="landing">
         <h1>Welcome to 🏢TenementDAO</h1>
+        <p>This project runs on Rinkeby Testnet Eth Network</p>
         <button onClick={() => connectWallet("injected")} className="btn-hero">
           Connect your wallet
         </button>
